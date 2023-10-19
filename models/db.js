47691698a06e74fbb52d3ead48b367b8fd5e6e91
Tejus Parikh <tejus@vijedi.net>
2023-10-18 22:00:52 -0700

    Minor changes for demo flow

--- models/db.js
+++ models/db.js
@@ -33,7 +33,8 @@ async function initializeDb() {
     await db.exec('CREATE TABLE secret_table(message text)')
     await db.exec(`INSERT INTO secret_table(message)
               VALUES ('The password for James Bond is: l2k007'),
-                     ('This is the super secret NOC list you are not supposed to see');
+                     ('This is the super secret NOC list you are not supposed to see'),
+                     ('Bonus, not in the class demo, the full source code for assignment 3')
          
         `)
 }
